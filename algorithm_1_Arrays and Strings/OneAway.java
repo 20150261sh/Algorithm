cee2381f3b19be1256bf6f73c5bf878a71225c9e
songhee <>
2018-05-19 01:03:06 +0900

    1. Arrays and Strings 1-5) One Away: There are three types of edits that can be performed on strings: insert a character, remove a character, or replace a character. Given two strings, write a function to check if they are one edit (or zero edits) away.

--- algorithm_1_Arrays and Strings/OneAway.java
+++ algorithm_1_Arrays and Strings/OneAway.java
@@ -1,3 +1,6 @@
+/* CRACKING the CODING INTERVIEW
+1. Arrays and Strings
+1-5) One Away: There are three types of edits that can be performed on strings: insert a character, remove a character, or replace a character. Given two strings, write a function to check if they are one edit (or zero edits) away.*/
 
 public class OneAway {
 
