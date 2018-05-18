3cae0d917a08a67ef34621ee5d4c607025a70cff
songhee <>
2018-05-19 01:01:48 +0900

    1. Arrays and Strings 1-4) Palindrome Permutation: Given a string, write a function to check if it is a permutation of a palin­ drome. A palindrome is a word or phrase that is the same forwards and backwards. A permutation is a rearrangement of letters. The palindrome does not need to be limited to just dictionary words.

--- algorithm_1_Arrays and Strings/Palindrome.java
+++ algorithm_1_Arrays and Strings/Palindrome.java
@@ -1,3 +1,7 @@
+/* CRACKING the CODING INTERVIEW
+1. Arrays and Strings
+1-4) Palindrome Permutation: Given a string, write a function to check if it is a permutation of a palin­ drome. A palindrome is a word or phrase that is the same forwards and backwards. A permutation is a rearrangement of letters. The palindrome does not need to be limited to just dictionary words. */
+
 import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.Scanner;
