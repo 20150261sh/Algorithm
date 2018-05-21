08875fe5f2cd13c163a87a8d2380e89bb925eb30
songhee <>
2018-05-22 01:47:17 +0900

    1-1 Is Unique

--- CRACKtheCODINGINTERVIEW/src/IsUnique.java
+++ CRACKtheCODINGINTERVIEW/src/IsUnique.java
@@ -9,7 +9,7 @@ public class IsUnique {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Hashtable<String, Integer> h = new Hashtable<String, Integer>();
-		String str = "apple";
+		String str = "aple";
 		for (int i = 0; i < str.length(); i++) {
 			if (h.containsKey(str.substring(i, i + 1))) {
 				System.out.println("String has same characters");
