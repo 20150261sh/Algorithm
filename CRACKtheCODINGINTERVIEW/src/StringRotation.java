d15136de7dfe22f47684a025f6f5a6fa7dcae1c0
songhee <>
2018-06-05 00:28:08 +0900

    1. Arrays and Strings 1-9) String Rotation:Assumeyou have a method isSubstringwhich checks if one word is a substring of another. Given two strings, sl and s2, write code to check if s2 is a rotation of sl using only one call to isSubstring (e.g.,"waterbottle" is a rotation of"erbottlewat").

--- CRACKtheCODINGINTERVIEW/src/StringRotation.java
+++ CRACKtheCODINGINTERVIEW/src/StringRotation.java
@@ -1,28 +1,28 @@
-/* CRACKING the CODING INTERVIEW
-1. Arrays and Strings
-1-9) String Rotation:Assumeyou have a method isSubstringwhich checks if one word is a substring of another. Given two strings, sl and s2, write code to check if s2 is a rotation of sl using only one call to isSubstring (e.g.,"waterbottle" is a rotation of"erbottlewat"). */
-
-import java.util.Scanner;
-
-public class StringRotation {
-	public static boolean stringRotation(String str1, String str2) {
-		int len = str1.length();
-		if(len > 0 && len != str2.length()) {
-			String s1s1 = str1 + str1;
-			return isSubString(s1s1,str2); //checks if one word is a substring of another: isSubString(s1,s2)
-		}
-		else
-			return false;
-	}
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Input string1: ");
-		String str1 = scan.nextLine();
-		System.out.println("Input string2: ");
-		String str2 = scan.nextLine();
-		stringRotation(str1, str2);
-		System.out.println(str1 + str2);
-	}
-}
+///* CRACKING the CODING INTERVIEW
+//1. Arrays and Strings
+//1-9) String Rotation:Assumeyou have a method isSubstringwhich checks if one word is a substring of another. Given two strings, sl and s2, write code to check if s2 is a rotation of sl using only one call to isSubstring (e.g.,"waterbottle" is a rotation of"erbottlewat"). */
+//
+//import java.util.Scanner;
+//
+//public class StringRotation {
+//	public static boolean stringRotation(String str1, String str2) {
+//		int len = str1.length();
+//		if(len > 0 && len != str2.length()) {
+//			String s1s1 = str1 + str1;
+//			return isSubString(s1s1,str2); //checks if one word is a substring of another: isSubString(s1,s2)
+//		}
+//		else
+//			return false;
+//	}
+//	
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		Scanner scan = new Scanner(System.in);
+//		System.out.println("Input string1: ");
+//		String str1 = scan.nextLine();
+//		System.out.println("Input string2: ");
+//		String str2 = scan.nextLine();
+//		stringRotation(str1, str2);
+//		System.out.println(str1 + str2);
+//	}
+//}
