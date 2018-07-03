054f643c0215d6e128d1b7569790a60eebf84199
songhee <>
2018-07-03 10:36:00 +0900

    4-1 Route Between Nodes: Use adjacency matrix

--- CRACKtheCODINGINTERVIEW/src/DirectedGraph.java
+++ CRACKtheCODINGINTERVIEW/src/DirectedGraph.java
@@ -1,51 +1,86 @@
-import java.util.ArrayList;
+/* CRACKING the CODING INTERVIEW
+4. Trees and Graphs
+4-1) Route Between Nodes: Given a directed graph, design an algorithm to find out whether there is a route between two nodes. 
+(1) Use adjacency matrix*/
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
-import java.util.Iterator;
 
 public class DirectedGraph {
-	// static int v;
-	// static int e;
+	static int v;
+	static int e;
+	static int[][] array;
 
-	static int nV; // 총 vertex 개수
-	static int nE; // 총 edge 개수
+	static void setArray() {
+		array = new int[v][v];
+		for (int i = 0; i < v; i++) {
+			for (int j = 0; j < v; j++)
+				array[i][j] = 0;
+		}
+	}
 
-public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in);
-    
-    nV = scan.nextInt();
-    nE = scan.nextInt();
-    ArrayList<ArrayList<Integer>> adList = new <ArrayList<Integer>> ArrayList();
-    
-    for(int i=0;i<nV;i++) adList.add(new<Integer> ArrayList());
-    //adList에 담을 리스트(각 vertex마다 인접한 vertext를 표현한 리스트)
-    
-    for(int i=0;i<nE;i++) {
-        int vertex1, vertex2;
-        vertex1 = scan.nextInt();
-        vertex2 = scan.nextInt();
-        
-        adList.get(vertex1).add(vertex2);
-    }
-    
-    
-    //인접리스트 출력
-    for(int i=0;i<nV;i++) {
-        Iterator<Integer> iter = adList.get(i).iterator();
-        System.out.print(i);
-        if(iter.hasNext()) System.out.print("-");
-        while(iter.hasNext()) System.out.print(iter.next() + " ");
-        System.out.println("");
-    }
-}
+	static void printArray() {
+		for (int i = 0; i < v; i++) {
+			for (int j = 0; j < v; j++)
+				System.out.print(array[i][j] + " ");
+			System.out.println();
+		}
+		System.out.println();
+	}
+	
+	static boolean routeNode() {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("-----Consider node's direct-----");
+		System.out.println("# Input node1's number: ");
+		int n1 = scan.nextInt();
+		System.out.println("# Input node2's number: ");
+		int n2 = scan.nextInt();
+		System.out.println("Node " + n1 + " ->" + " " + n2 + " 's route: ");
+		
+		if(array[n1-1][n2-1] == 1)
+			return true;
+		else 
+			return false;
+	}
 
-	// public static void main(String[] args) {
-	// // TODO Auto-generated method stub
-	// Scanner scan = new Scanner(System.in);
-	// System.out.println("Number of vertex: ");
-	// v = scan.nextInt();
-	// System.out.println("Number of edge: ");
-	// e = scan.nextInt();
-	// ArrayList<ArrayList<Integer>> list = new <ArrayList<Integer>> ArrayList();
-	// }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BufferedReader input = null;
+		try {
+			input = new BufferedReader(new FileReader("/Users/songhee/Desktop/input.txt"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		String s = null;
+		int count = 0;
+		try {
+			while ((s = input.readLine()) != null) {
+				count++;
+				if (count == 1)
+					v = Integer.parseInt(s);
+				else if (count == 2)
+					e = Integer.parseInt(s);
+				else {
+					if (count == 3)
+						setArray();
+					String[] line = s.split(" ");
+					int i = Integer.parseInt(line[0]);
+					int j = Integer.parseInt(line[1]);
+					array[i - 1][j - 1] = 1;
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		printArray();
+		System.out.println(routeNode());
+	}
 
 }
