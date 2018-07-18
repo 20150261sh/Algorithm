ca74e80f30faa9fb83f65adebbeb7045f37c53bc
20150261sh <20150261sh@gmail.com>
2018-07-18 22:44:09 +0900

    bst

--- CRACKtheCODINGINTERVIEW/src/BSTree.java
+++ CRACKtheCODINGINTERVIEW/src/BSTree.java
@@ -1,119 +1,153 @@
 public class BSTree {
-   TreeNode root = null;
-   int number = 0;
+	TreeNode root = null;
+	int number = 0;
 
-   void printBST() {
-      preorder(root);
-      System.out.println();
-   }
+	void printBST() {
+		preorder(root);
+		System.out.println();
+	}
+	
+	boolean IsNoChild(TreeNode targetNode) {
+		if(targetNode.left == null && targetNode.right == null)
+			return true;
+		else 
+			return false;
+	}
 
-   TreeNode insertKey(TreeNode root, int input) {
-      TreeNode tmpNode = root;
-      TreeNode newNode = new TreeNode(input);
-      if (tmpNode == null)
-         return newNode;
-      else if (tmpNode.data > newNode.data) {
-         tmpNode.left = insertKey(tmpNode.left, input);
-         return tmpNode;
-      } else if (tmpNode.data < newNode.data) {
-         tmpNode.right = insertKey(tmpNode.right, input);
-         return tmpNode;
-      } else
-         return tmpNode;
-   }
+	TreeNode insertKey(TreeNode root, int input) {
+		TreeNode tmpNode = root;
+		TreeNode newNode = new TreeNode(input);
+		if (tmpNode == null)
+			return newNode;
+		else if (tmpNode.data > newNode.data) {
+			tmpNode.left = insertKey(tmpNode.left, input);
+			return tmpNode;
+		} else if (tmpNode.data < newNode.data) {
+			tmpNode.right = insertKey(tmpNode.right, input);
+			return tmpNode;
+		} else
+			return tmpNode;
+	}
 
-   void insertBST(int input) {
-      if (root == null) {
-         root = new TreeNode(input);
-         number++;
-         return;
-      }
-      root = insertKey(root, input);
-      number++;
-   }
+	void insertBST(int input) {
+		if (root == null) {
+			root = new TreeNode(input);
+			number++;
+			return;
+		}
+		root = insertKey(root, input);
+		number++;
+	}
 
-   void deleteNode(int delVal) {
-      int count = 0;
-      TreeNode tmpNode = root;
-      TreeNode parentNode = tmpNode;
-      TreeNode delNode = null;
+	void deleteNode(int delVal) {
+		int count = 0;
+		TreeNode tmpNode = root;
+		TreeNode parentNode = tmpNode;
+		TreeNode delNode = null;
 
-      // 삭제할 노드 탐색
-      while (true) {
-         if (tmpNode.data == delVal) {
-            delNode = tmpNode;
-            break;
-         }
-         if (tmpNode.data > delVal) {
-            if (count > 0)
-               parentNode = tmpNode;
-            tmpNode = tmpNode.left;
-            count++;
-         } else if (tmpNode.data < delVal) {
-            if (count > 0)
-               parentNode = tmpNode;
-            tmpNode = tmpNode.right;
-            count++;
-         }
-         if (this.number == count) {
-            System.out.println("The delVal is not exist.");
-            return;
-         }
-      }
+		// 삭제할 노드 탐색
+		while (true) {
+			if (tmpNode.data == delVal) {
+				delNode = tmpNode;
+				break;
+			}
+			if (tmpNode.data > delVal) {
+				if (count > 0)
+					parentNode = tmpNode;
+				if(IsNoChild(tmpNode)) {
+					System.out.println("The delVal is not exist.");
+					return;
+				}
+				tmpNode = tmpNode.left;
+				count++;
+			} else if (tmpNode.data < delVal) {
+				if (count > 0)
+					parentNode = tmpNode;
+				if(IsNoChild(tmpNode)) {
+					System.out.println("The delVal is not exist.");
+					return;
+				}
+				tmpNode = tmpNode.right;
+				count++;
+			}
+		}
+	
+		System.out.println("delNode value is " + delNode.data);
+		// 실제 삭제
+		if (delNode.left == null && delNode.right == null) { // 자식이 없는 노드 삭제
+			if (parentNode.left.data == delNode.data)
+				parentNode.left = null;
+			else
+				parentNode.right = null;
+		} else if (delNode.left == null || delNode.right == null) { // 자식 1개
+			if (delNode.left != null) {
+				if (parentNode.left == delNode)
+					parentNode.left = delNode.left;
+				else if (parentNode.right == delNode)
+					parentNode.right = delNode.left;
+			} else if (delNode.right != null) {
+				if (parentNode.left == delNode)
+					parentNode.left = delNode.right;
+				else if (parentNode.right == delNode)
+					parentNode.right = delNode.right;
+			}
 
-      // 실제 삭제
-      if (delNode.left == null && delNode.right == null) { // 자식이 없는 노드 삭제
-         if (parentNode.left.data == delNode.data)
-            parentNode.left = null;
-         else
-            parentNode.right = null;
-      } else if (delNode.left == null || delNode.right == null) { // 자식 1개
-         if (delNode.left != null) {
-            if (parentNode.left == delNode)
-               parentNode.left = delNode.left;
-            else if (parentNode.right == delNode)
-               parentNode.right = delNode.left;
-         } else if (delNode.right != null) {
-            if (parentNode.left == delNode)
-               parentNode.left = delNode.right;
-            else if (parentNode.right == delNode)
-               parentNode.right = delNode.right;
-         }
-      } else if (delNode.left != null && delNode.right != null) { // 자식 2개
-         tmpNode = delNode.right;
-         if (delNode == root) {
-            tmpNode.left = delNode.left;
-            root = tmpNode;
-         }
-         if (parentNode.left == delNode) {
-            parentNode.left = tmpNode;
-         } else if (parentNode.right == delNode) {
-            parentNode.right = tmpNode;
-         }
-      }
-   }
+		} else if (delNode.left != null && delNode.right != null) { // 자식 2개
+			tmpNode = delNode.right;
+			if (parentNode.data == delNode.data) {
+				if (tmpNode.left != null) {
+					TreeNode child = tmpNode.left;
+					if (delNode.left.data < child.data) {
+						tmpNode.left = delNode.left;
+						if(tmpNode.left.right != null) {
+							if(tmpNode.left.right.data < child) {
+								
+							}
+						}
+						else
+							tmpNode.left.right = child;
+					}
+				} else {
+					tmpNode.left = root.left;
+				}
+				root = tmpNode;
+			} else if (parentNode.left == delNode) {
+				parentNode.left = tmpNode;
+			} else if (parentNode.right == delNode) {
+				parentNode.right = tmpNode;
+			}
+			if (tmpNode.left != null) {
+				TreeNode child = tmpNode.left;
+				if (delNode.left.data < child.data) {
+					tmpNode.left = delNode.left;
+					tmpNode.left.right = child;
+				} else
+					child.right = delNode.left;
+			}
+		}
+	}
 
-   void preorder(TreeNode root) {
-      if (root != null) {
-         System.out.print(root.data + " ");
-         preorder(root.left);
-         preorder(root.right);
-      }
-   }
+	void preorder(TreeNode root) {
+		if (root != null) {
+			System.out.print(root.data + " ");
+			preorder(root.left);
+			preorder(root.right);
+		}
+	}
 
-   void inorder(TreeNode root) {
-      if (root != null) {
-         inorder(root.left);
-         System.out.print(root.data + " ");
-         inorder(root.right);
-      }
-   }
+	void inorder(TreeNode root) {
+		if (root != null) {
+			inorder(root.left);
+			System.out.print(root.data + " ");
+			inorder(root.right);
+		}
+	}
 
-   void postorder(TreeNode root) {
-      if (root != null) {
-         postorder(root.left);
-         postorder(root.right);
-         System.out.print(root.data + " ");
-      }
-   }
+	void postorder(TreeNode root) {
+		if (root != null) {
+			postorder(root.left);
+			postorder(root.right);
+			System.out.print(root.data + " ");
+		}
+	}
 }
--- CRACKtheCODINGINTERVIEW/src/BSTreeTest.java
+++ CRACKtheCODINGINTERVIEW/src/BSTreeTest.java
@@ -4,10 +4,27 @@ public class BSTreeTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		BSTree bst = new BSTree();
-		bst.insertBST(8);
-		bst.insertBST(10);
+		bst.insertBST(12);
+		bst.insertBST(5);
+		bst.insertBST(15);
 		bst.insertBST(3);
-		bst.printBST();
+		bst.insertBST(7);
+		bst.insertBST(13);
+		bst.insertBST(17);
+		bst.insertBST(16);
+		bst.insertBST(19);
+//		bst.printBST();
+//		System.out.println(bst.root.data);
+//		System.out.println(bst.root.left.data);
+//		System.out.println(bst.root.left.left.data);
+//		System.out.println(bst.root.left.right.data);
+//		System.out.println(bst.root.right.data);
+//		System.out.println(bst.root.right.right.data);
+//		System.out.println(bst.root.right.right.left.data);
+//		System.out.println(bst.root.right.right.right.data);
+		bst.deleteNode(12);
+		bst.deleteNode(5);
+		System.out.println("val " + bst.root.left.data);
 	}
 
 }
